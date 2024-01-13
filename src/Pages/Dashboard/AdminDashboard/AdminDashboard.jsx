49aa1de12c4c0c8e63e37e25d2f3bdcdec80e13e
mmahbubr0823 <mahbubfdtc@gmail.comm>
2024-01-13 12:45:45 +0600

    pie chart created

--- src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ src/Pages/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,36 +1,49 @@
 import useBioData from '../../../Hooks/useBioData/useBioData';
 import { useEffect, useState } from 'react';
+import { PieChart, Pie, Tooltip, Legend, Cell } from 'recharts';
+
 import usePremium from '../../../Hooks/usePremium/usePremium';
 const AdminDashboard = () => {
     const [data] = useBioData();
     const [premiumUser] = usePremium();
-
     const [male, setMale] = useState([]);
     const [female, setFemale] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         const filter = data.filter(res => res.BiodataType === 'Male');
         setMale(filter);
         // female data 
         const filter2 = data.filter(res => res.BiodataType === 'Female');
         setFemale(filter2);
     }, [data])
+    const data01 = [
+        { name: 'Total Bio Data', value: data.length },
+        { name: ' Male Data', value: male.length },
+        { name: ' Female Data', value: female.length },
+        { name: ' Premium User', value: premiumUser.length },
+    ];
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
     return (
         <div>
             <h1 className='text-3xl text-center font-semibold mb-5'>Bio Data stat at a glance:</h1>
-            <div className='grid grid-cols-2 gap-4 px-5'>
-                <div className='bg-[#eaf46b] w-96 p-12 rounded-lg'>
-                    <h1 className='text-2xl'>Total Bio Data: { data.length}</h1>
-                </div>
-                <div className='bg-[#f46b70] w-96 p-12 rounded-lg'>
-                    <h1 className='text-2xl'>Total Male Data: { male.length}</h1>
-                </div>
-                <div className='bg-[#f078d2] w-96 p-12 rounded-lg'>
-                    <h1 className='text-2xl'>Total Female Data: { female.length}</h1>
-                </div>
-                <div className='bg-[#f078d2] w-96 p-12 rounded-lg'>
-                    <h1 className='text-2xl'>Total Premium User: { premiumUser.length}</h1>
-                </div>
-            </div>
+            <PieChart width={400} height={400}>
+                <Pie
+                    dataKey="value"
+                    isAnimationActive={false}
+                    data={data01}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label={true}
+                >
+                    {data01.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                    ))}
+                </Pie>
+                <Legend></Legend>
+                <Tooltip />
+            </PieChart>
+
         </div>
     );
 };
